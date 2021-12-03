037bf1689e44da7f1ac06b82059e2b468115456d
insuby <36872552+insuby@users.noreply.github.com>
2021-12-03 16:50:17 +0300

    Update index.js

--- src/components/Form/Comment/index.js
+++ src/components/Form/Comment/index.js
@@ -1,9 +1,9 @@
 import './index.scss';
 import React, {useContext, useState} from 'react';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faTimes} from "@fortawesome/free-solid-svg-icons";
-import {Context} from "../../../index";
-import {useParams} from "react-router-dom/cjs/react-router-dom";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faTimes} from '@fortawesome/free-solid-svg-icons';
+import {Context} from '../../../index';
+import {useParams} from 'react-router-dom/cjs/react-router-dom';
 
 const FormComment = ({head}) => {
     const {modal, commentsStore} = useContext(Context)
@@ -24,28 +24,28 @@ const FormComment = ({head}) => {
     }
 
     return (
-        <div className={"form-comment"}>
-            <div className={"form-comment__head"}>
+        <div id={'form-comment'} className={'form-comment'}>
+            <div className={'form-comment__head'}>
                 <p>{head}</p>
-                <span className={"form-comment__close"}
+                <span className={'form-comment__close'}
                       onClick={() => {
                           modal.setShow(false)
                       }}>
                         <FontAwesomeIcon icon={faTimes}/>
                 </span>
             </div>
-            <div className={"form-comment__body"}>
-                <label htmlFor="author">Your name:</label>
-                <input onChange={onChange} type="text" name="author" id="author" value={form.author}/>
-                <label htmlFor="form-comment">Your comment:</label>
-                <textarea autoFocus={true} name="comment" id="comment" cols="30"
-                          onChange={onChange} rows="10" required/>
+            <div className={'form-comment__body'}>
+                <label htmlFor='author'>Your name:</label>
+                <input onChange={onChange} type='text' name='author' id='author' value={form.author}/>
+                <label htmlFor='form-comment'>Your comment:</label>
+                <textarea autoFocus={true} name='comment' id='comment' cols='30'
+                          onChange={onChange} rows='10' required/>
             </div>
-            <div className={"form-comment__foot"}>
-                <button className={"btn form-comment__btn form-comment__btn_submit"}
-                        children={"submit"} onClick={setComment}/>
-                <button className={"btn form-comment__btn form-comment__btn_close"}
-                        children={"cancel"}
+            <div className={'form-comment__foot'}>
+                <button className={'btn form-comment__btn form-comment__btn_submit'}
+                        children={'submit'} onClick={setComment}/>
+                <button className={'btn form-comment__btn form-comment__btn_close'}
+                        children={'cancel'}
                         onClick={() => {
                             modal.setShow(false)
                         }}/>
