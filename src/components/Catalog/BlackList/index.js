3423849716b5750ce853a30cb12728cdf333400f
insuby <36872552+insuby@users.noreply.github.com>
2021-12-03 16:51:21 +0300

    Update index.js

--- src/components/Catalog/BlackList/index.js
+++ src/components/Catalog/BlackList/index.js
@@ -1,8 +1,7 @@
 import './index.scss'
 import React, {useContext} from 'react';
-import {Context} from "../../../index";
-import {observer} from "mobx-react-lite";
-import {ucFirst} from "../../../utils/helpers";
+import {Context} from '../../../index';
+import {observer} from 'mobx-react-lite';
 
 const RemovedMovies = observer(() => {
     const {catalog} = useContext(Context)
@@ -14,16 +13,14 @@ const RemovedMovies = observer(() => {
     return (
         <div className={'black-list'}>
             <ul className={'shadow-wrapper'}>
-                {
-                    catalog.blackList.length ?
-                        catalog.blackList.map(({title, id}, index) => {
-                            return <li key={id} id={id} onClick={onClick} title={'click to remove from the black list'}>
-                                <span>{index + 1}</span>
-                                <span>{ucFirst(title)}</span>
-                            </li>
-                        })
-                        : <li>Black list is empty</li>
-                }
+                {catalog.blackList.length ?
+                 catalog.blackList.map(({title, id}, index) => {
+                     return <li key={id} id={id} onClick={onClick}
+                                title={'click to remove from the black list'}>
+                         <span>{index + 1}</span>
+                         <span className={'black-list__title strokes-count-2'}>{title}</span>
+                     </li>
+                 }) : <li>Black list is empty</li>}
             </ul>
         </div>
     );
