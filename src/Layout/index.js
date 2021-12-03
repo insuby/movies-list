2016d57e872bfb08efa8fa057a6829d38e7818b3
insuby <36872552+insuby@users.noreply.github.com>
2021-12-03 16:50:31 +0300

    Update index.js

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -2,22 +2,24 @@ import './index.scss';
 import React from 'react';
 import {Context} from "../index";
 import AppRouter from "../components/AppRouter";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Container} from "react-bootstrap";
 import CatalogStore from "../store/catalog";
 import CommentsStore from "../store/comments";
 import LoaderStore from "../store/loader";
 import ModalStore from "../store/modal";
+import Modal from '../components/Modal';
+import Loader from '../components/Loader';
 
 const Layout = () => {
     return (
         <Context.Provider value={{
             catalog: new CatalogStore(),
             commentsStore: new CommentsStore(),
-           loader: new LoaderStore(),
+            loader: new LoaderStore(),
             modal: new ModalStore(),
         }}>
-            <BrowserRouter>
+            <HashRouter>
                 <header>
                     <nav/>
                 </header>
@@ -29,7 +31,7 @@ const Layout = () => {
                 <footer/>
                 <Loader/>
                 <Modal/>
-            </BrowserRouter>
+            </HashRouter>
         </Context.Provider>
     );
 };
