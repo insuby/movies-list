690e7524ca9f5363e80079701ce97ddf13d539b4
insuby <36872552+insuby@users.noreply.github.com>
2021-12-03 14:58:10 +0300

    Update index.js

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -21,20 +21,20 @@ const Product = observer(() => {
             <div className="product__image">
                 <img src={large_cover_image} alt={title}/>
             </div>
-            <div className="product__info">
-                <h1 className={'product__info__name'}>{title}</h1>
-                <p className={'product__info__genre'}>{genres && genres.map(genre => <span
+            <div className="product__info info">
+                <h1 className={'info__name'}>{title}</h1>
+                <p className={'info__genre'}>{genres && genres.map(genre => <span
                     key={genre}>{genre}</span>)}</p>
-                <button className={"product__info__btn"} onClick={() => {
+                <button className={"info__btn"} onClick={() => {
                     modal.setShow(true)
                 }}>
                     <FontAwesomeIcon icon={faComment}/>
                     leave comment
                 </button>
-                <p className={'product__info__uploaded'}>Uploaded: {date_uploaded}</p>
-                <p className={'product__info__desc'}>{description_full}</p>
+                <p className={'info__uploaded'}>Uploaded: {date_uploaded}</p>
+                <p className={'info__desc'}>{description_full}</p>
             </div>
-            <div className={"product__comments"}>
+            <div className={"product__comments comment"}>
                 <h3>Comments</h3>
                 {
                     commentsStore.comments[id] ?
@@ -42,14 +42,14 @@ const Product = observer(() => {
                             {
                                 commentsStore.comments[id].map(({author, comment, commentID}) => {
                                     return <li key={commentID}>
-                                            <span className={"product__comments__close"}
+                                            <span className={"product__comment__close"}
                                                   onClick={(event) => {
                                                       commentsStore.removeComment(+id, commentID)
                                                   }}>
                                             <FontAwesomeIcon icon={faTimes}/>
                                         </span>
-                                        <span className={"product__comments__text"} children={comment}/>
-                                        <span className={"product__comments__author"} children={author}/>
+                                        <span className={"comment__text"} children={comment}/>
+                                        <span className={"comment__author"} children={author}/>
                                     </li>
                                 })
                             }
